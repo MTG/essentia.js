c6371bcc15af7c186bf2299686964de299417af2
Albin <albin.a.correya@gmail.com>
2019-08-21 17:07:15 +0200

    removed unnecessary files

--- build_wasm.sh
+++
@@ -1,9 +0,0 @@
-
-
-LIB_DIR=$EMSCRIPTEN/system/local/lib
-$SRC_EMSENTIA_DIR=./src/cpp
-$BUILD_EMSENTIA_DIR=./build
-
-emcc  -Oz -c $SRC_B_CPP/emscripten.cpp $SRC_B_CPP/essentiamin.cpp -o $BUILD_EMSENTIA_DIR/emsentia.bc
-
-emcc -Oz BUILD_EMSENTIA_DIR/emsentia.bc $LIB_DIR/libessentia.a $LIB_DIR/libfftw3f.a -o $BUILD_EMSENTIA_DIR/essentiamin.js
