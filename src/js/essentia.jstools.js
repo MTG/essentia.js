c05dec6def25d0590cc4938a56f3ea35f6499962
albincorreya <albin.a.correya@gmail.com>
2020-01-21 12:05:37 +0100

    add js pre-build src files, essentiajstools

---
+++ src/js/essentia.jstools.js
@@ -0,0 +1,40 @@
+
+/*
+Class with utility methods to use essentia.js
+*/
+class EssentiaJsTools {
+
+	constructor(essentiaWasmModule) {
+		this._module = essentiaWasmModule;
+	}
+
+	// copy the contents of a float 32 js typed array into a std::vector<float>.
+	typedFloatArray2Vector = function(typedArray) {
+		let vect = new this._module.VectorFloat();
+    for (let i=0; i<typedArray.length; i++) {
+      if (typeof typedArray[i] === 'undefined') {
+      	vect.push_back(0);
+        }
+        else {
+          vect.push_back(typedArray[i]);
+        }
+    }
+    return vect; 
+  }
+
+  // copy the contents of a std::vector<float> into a float 32 js typed array. 
+	vector2typedFloat32Array = function(vect) {
+		const typedArray = new Float32Array(vec.size());
+		for (var i=0; i < vect.size(); i++) {
+			typedArray[i] = vect.get(i); 
+			typedArray[i] = parseFloat(typedArray[i].toFixed(2));
+		}
+		return typedArray
+	}
+
+	// add your essentia.js utility funcs here
+
+}
+
+
+export { EssentiaJsTools };
