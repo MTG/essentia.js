230b8bbbbfed42a99904b44adcf20f936f59c35a
Albin <albin.a.correya@gmail.com>
2020-02-02 03:40:36 +0100

    update travis config and deploy scripts

--- travis/deploy_builds.sh
+++ travis/deploy_builds.sh
@@ -5,6 +5,6 @@ for js in essentia*.js; do
     curl --upload-file "$js"  https://transfer.sh/"$js" -w "\n"
 done
 
-# for wasmfile in essentia*.wasm; do
-#     curl --upload-file "$wasmfile"  https://transfer.sh/"$wasmfile" -w "\n"
-# done
+for wasmfile in essentia*.wasm; do
+    curl --upload-file "$wasmfile"  https://transfer.sh/"$wasmfile" -w "\n"
+done
