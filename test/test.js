51830f70a615589109ae6e00b1beaca5419719c0
Albin <albin.a.correya@gmail.com>
2020-02-25 13:10:46 +0100

    added a base test template

--- test/test.js
+++ test/test.js
@@ -1,16 +1,16 @@
 // Tests for essentia.js 
 
 var chai = require('chai');
-var ess = require('essentia.js');
+var esLib = require('essentiajs');
 
 describe('essentia.js', function() {
   // TODO: write more concise tests
   it('should sucessfully find all the essentia class and methods', function() {
-    chai.assert.hasAllKeys(ess.Essentia, ['EssentiaJS', 
-                                          'VectorFloat', 
-                                          'VectorString', 
-                                          'VectorDouble', 
-                                          'VectorVectorFloat']);
+    chai.assert.hasAllKeys(esLib, ['EssentiaJS', 
+                                   'VectorFloat', 
+                                   'VectorString', 
+                                   'VectorDouble', 
+                                   'VectorVectorFloat']);
   });
 
   // TODO: add more tests here
