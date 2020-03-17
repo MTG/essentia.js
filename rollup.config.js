2bedbcc26cdbd2a783cf4351465332e3b56a2fff
Albin <albin.a.correya@gmail.com>
2020-03-17 20:35:00 +0100

    add rollup config for building typescript assests

---
+++ rollup.config.js
@@ -0,0 +1,62 @@
+import typescript from 'rollup-plugin-typescript2';
+import {terser} from "rollup-plugin-terser";
+
+export default [{
+  input: 'src/typescript/essentia.js.core.ts', // our source file
+  output: [
+    {
+    file: 'dist/essentia.js-core-module.js',
+    format: 'es' // the preferred format
+    },
+    {
+    file: 'dist/essentia.js-core.js',
+    format: 'iife',
+    name: 'Essentia' // the global which can be used in a browser
+    }
+  ],
+  plugins: [
+    typescript({
+    typescript: require('typescript'),
+    }),
+    terser() // minifies generated bundles
+  ]
+}, {
+ input: 'src/typescript/essentia.js.plot.ts', // our source file
+ output: [
+  {
+   file: 'dist/essentia.js-plot-module.js',
+   format: 'es' // the preferred format
+  },
+  {
+   file: 'dist/essentia.js-plot.js',
+   format: 'iife',
+   name: 'EssentiaPlot' // the global which can be used in a browser
+  }
+ ],
+ plugins: [
+  typescript({
+   typescript: require('typescript'),
+  }),
+  terser() // minifies generated bundles
+ ]
+}, {
+  input: 'src/typescript/essentia.js.models.ts', // our source file
+  output: [
+   {
+    file: 'dist/essentia.js-models-module.js',
+    format: 'es' // the preferred format
+   },
+   {
+    file: 'dist/essentia.js-models.js',
+    format: 'iife',
+    name: 'EssentiaModels' // the global which can be used in a browser
+   }
+  ],
+  plugins: [
+   typescript({
+    typescript: require('typescript'),
+   }),
+   terser() // minifies generated bundles
+  ]
+ },
+];
