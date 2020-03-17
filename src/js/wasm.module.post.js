b8c110c62fc1d9b31a54048854005ab6dfac36b5
Albin <albin.a.correya@gmail.com>
2020-03-17 20:13:36 +0100

    closes #13, encapsulate array conversion funcs inside the wasm builds

--- src/js/wasm.module.post.js
+++
@@ -1,4 +0,0 @@
-// EXPORT_ES6 option does not work as described at
-// https://github.com/emscripten-core/emscripten/issues/6284, so we have to
-// manually add this to the final builds.
-export default Module;
