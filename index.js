88e1b8428aecf75700dcae8e42ca8fb410a6af80
Albin <albin.a.correya@gmail.com>
2020-07-14 16:47:43 +0200

    closes #29, rename Essentia WASM backend global namespace to "EssentiaWASM"

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict"
 // make entrypoint to ES6 class using esm
 require = require('esm')(module)
-var Essentia = require('./src/js/es6module.main.js');
+const Essentia = require('./src/js/es6module.main.js');
 module.exports = Essentia.default;
--- src/js/es6module.main.js
+++ src/js/es6module.main.js
@@ -1,5 +1,5 @@
-import { EssentiaModule } from "../../dist/essentia-wasm.module";
+import { EssentiaWASM } from "../../dist/essentia-wasm.module";
 import Essentia from "../../dist/essentia.js-core.es";
 // export as default for entry point to node package with the js wrapper
-let EssentiaNode = new Essentia(EssentiaModule);
+const EssentiaNode = new Essentia(EssentiaWASM);
 export default EssentiaNode;
