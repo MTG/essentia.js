07f3083fcc7fc788a598f4d13ca69882e6d60b8c
Albin <albin.a.correya@gmail.com>
2020-07-20 11:53:05 +0200

    remove ceiling of float values in vectorToArray method

--- src/js/wasm.es6module.post.js
+++ src/js/wasm.es6module.post.js
@@ -4,7 +4,7 @@ Module['vectorToArray'] = function(vect) {
   const typedArray = new Float32Array(vect.size());
     for (var i=0; i < vect.size(); i++) {
       typedArray[i] = vect.get(i); 
-      typedArray[i] = parseFloat(typedArray[i].toFixed(2));
+      // typedArray[i] = parseFloat(typedArray[i].toFixed(2));
     }
   return typedArray;
 }
