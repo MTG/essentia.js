97f71bc6a67f15480f770b1143c1f1050cbf5051
albincorreya <albin.a.correya@gmail.com>
2020-12-17 11:51:17 +0100

    expose Essentia and EssentiaWASM seprately for nodejs entrypoints

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict"
 // make entrypoint to ES6 class using esm
 require = require('esm')(module)
-const Essentia = require('./src/js/es6module.main.js');
-module.exports = Essentia.default;
+const EssentiaPkg = require('./src/js/es6module.main.js');
+module.exports = EssentiaPkg;
--- src/js/es6module.main.js
+++ src/js/es6module.main.js
@@ -1,5 +1,5 @@
 import { EssentiaWASM } from "../../dist/essentia-wasm.module";
 import Essentia from "../../dist/essentia.js-core.es";
 // export as default for entry point to node package with the js wrapper
-const EssentiaNode = new Essentia(EssentiaWASM);
-export default EssentiaNode;
+// const EssentiaNode = new Essentia(EssentiaWASM);
+export {Essentia, EssentiaWASM};
