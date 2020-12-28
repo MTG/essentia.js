7c5b9ebccf2023288479cb5128c4b43cc0aa15f4
albincorreya <albin.a.correya@gmail.com>
2020-12-28 02:57:01 +0100

    fix model path in ml example

--- examples/machinelearning/inference-worker.js
+++ examples/machinelearning/inference-worker.js
@@ -6,10 +6,10 @@ importScripts(
   "../../dist/essentia.js-model.umd.js"
 );
 
-const modelURL = "../../models/msd-musicnn-1/model.json"
+const modelURL = "models/msd-musicnn-1/model.json"
 const musiCNN = new EssentiaModel.TensorflowMusiCNN(tf, modelURL);
 
-musiCNN.initialize().then(() => console.log("tjs model ready..."));
+musiCNN.initialize().then(() => console.log("essentia-tfjs model ready..."));
 
 self.onmessage = e => {
   musiCNN.predict(e.data, true)
