2f6d67844f951f2c99e6c41c82001d5b364c3538
albincorreya <albin.a.correya@gmail.com>
2021-06-21 09:21:55 +0200

    closes #50 , expose add-on modules to npm package entrypoint

--- src/js/wasm.umd.post.js
+++ src/js/wasm.umd.post.js
@@ -2,8 +2,10 @@ Module['vectorToArray'] = function(vect) {
   if (!vect) { throw "Null input"};
   if (vect.size() == 0) { throw "Empty vector input"};
   const typedArray = new Float32Array(vect.size());
-  for (var i=0; i < vect.size(); i++) {
-    typedArray[i] = vect.get(i); 
-  }
+    for (var i=0; i < vect.size(); i++) {
+      typedArray[i] = vect.get(i); 
+    }
   return typedArray;
 }
+// manually add this to the final builds.
+exports.EssentiaWASM = Module;
