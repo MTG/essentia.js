f740cbc1f49f360704f40d0636f6ee2117da9488
Jorge Marcos <jorge.marcosfer@gmail.com>
2021-08-30 18:50:54 +0200

    project structure with BootstrapVue

---
+++ examples/onsets/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ examples/onsets/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
