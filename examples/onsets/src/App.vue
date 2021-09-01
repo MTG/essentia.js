ab802286e9bb537ef7957a08bade963627eb0392
Jorge Marcos <jorge.marcosfer@gmail.com>
2021-09-01 17:50:15 +0200

    instructions welcome modal

--- examples/onsets/src/App.vue
+++ examples/onsets/src/App.vue
@@ -1,19 +1,33 @@
 <template>
-  <div id="app">
-  </div>
+  <main>
+    <!-- using placeholder GIFs -->
+    <instructions-modal img-link="https://media.giphy.com/media/eDOsXo70WqUU0/giphy.gif,
+                                    https://media.giphy.com/media/AUlBq9xqbWu0E/giphy.gif,
+                                    https://media.giphy.com/media/LSpDvtSVoq4yA/giphy.gif"
+                        @closed="instructionsClosed=true">
+      <template slot="header">Audio slicer fun!</template>
+      <template slot="lead">Create weighted combinations of multiple onset detection functions for different audio segmentation results.</template>
+      <template slot="dismiss">Got it!</template>
+    </instructions-modal>
+  </main>
 </template>
 
 <script>
+import InstructionsModal from './components/InstructionsModal.vue';
+
 export default {
   name: 'app',
+  components: { InstructionsModal },
   data () {
     return {
-      
+      instructionsClosed: false 
     }
   }
 }
 </script>
 
 <style lang="scss">
-
+  main {
+    height: 100vh;
+  }
 </style>
--- examples/onsets/src/main.js
+++ examples/onsets/src/main.js
@@ -1,18 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(BootstrapVueIcons);
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
