8487b8c6964cf535bb54d4f095f509a183db4bd1
Jorge Marcos <jorge.marcosfer@gmail.com>
2021-10-20 17:28:43 +0200

    fix two columns of fs results

--- examples/onsets/src/components/FreesoundResults.vue
+++ examples/onsets/src/components/FreesoundResults.vue
@@ -1,9 +1,11 @@
 <template>
-    <b-form-radio-group v-model="selected" class="list-container flex-column flex-wrap justify-content-center rounded border-0 bg-light py-1"> 
-        <li v-for="sound, idx in sounds" :key="idx" class="mx-auto">
-            <iframe frameborder="0" scrolling="no" :src="freesoundEmbedURL(sound.id)" width="375" height="25"></iframe>
-            <b-form-radio class="select-radio" @change="handleSelect(sound)"></b-form-radio>
-        </li>
+    <b-form-radio-group v-model="selected" class="row rounded border-0 bg-light py-1 w-100 mx-0"> 
+        <div class="col-sm px-0 w-100" v-for="col, colIdx in soundColumns" :key="colIdx">
+            <li v-for="sound, sndIdx in col" :key="sndIdx" class="mx-auto">
+                <iframe frameborder="0" scrolling="no" :src="freesoundEmbedURL(sound.id)" width="375" height="25"></iframe>
+                <b-form-radio class="select-radio" @change="handleSelect(sound)"></b-form-radio>
+            </li>
+        </div>
     </b-form-radio-group>
 </template>
 
@@ -13,8 +15,19 @@ import EventBus from '../core/event-bus';
 export default {
     data () {
         return {
-            sounds: null,
-            selected: ""
+            sounds: [],
+            selected: "",
+            cols: 2
+        }
+    },
+    computed: {
+        soundColumns () {
+            let columns = [];
+            let mid = Math.ceil(this.sounds.length / this.cols);
+            for (let col = 0; col < this.cols; col++) {
+                columns.push(this.sounds.slice(col * mid, col * mid + mid));
+            }
+            return columns;
         }
     },
     methods: {
@@ -40,10 +53,6 @@ export default {
 <style scoped>
     li {
         list-style: none;
-    }
-
-    .list-container {
-        max-height: 45%;
     }
 
     .select-radio {
