f855be415f9cb02d695595cdce0aa41f413372a6
Jorge Marcos <jorge.marcosfer@gmail.com>
2022-02-17 19:05:43 +0100

    add automatic audio loading to main for testing

--- examples/demos/audio-metering/src/main.js
+++ examples/demos/audio-metering/src/main.js
@@ -2,3 +2,27 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import { audioEngine } from './audio/engine.js';
+const songURLs = import.meta.glob('./assets/testSongs/*.mp3');
+
+
+async function retrieveTestSongs () {
+    let songs = [];
+    for (const path in songURLs) {
+        if (path.includes('Deep Mystery')) {
+            let url = await songURLs[path]();
+            let song = await fetch(url.default).then(resp => resp.blob());
+            songs.push(song);
+            break;
+        }
+    }
+    return songs;
+}
+
+window.onload = async () => {
+    let songs = await retrieveTestSongs();
+    let analysis = await audioEngine.batchProcess(songs);
+    console.log('analysis finished');
+};
+
