3f9b6c5da1cc7cd71487b5dd8cac03f8d614a419
Jorge Marcos <jorge.marcosfer@gmail.com>
2022-02-23 20:05:21 +0100

    switch to vue2for vuetify support

--- examples/demos/audio-metering/vite.config.js
+++ examples/demos/audio-metering/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { createVuePlugin } from 'vite-plugin-vue2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [createVuePlugin()],
   test: {
     environment: 'happy-dom'
   }
