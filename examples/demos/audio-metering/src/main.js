a930afb9fa2f0e4f115e91bfa406df5741277862
Jorge Marcos <jorge.marcosfer@gmail.com>
2022-03-01 20:31:16 +0100

    add vuetify in vite config and main

--- examples/demos/audio-metering/src/main.js
+++ examples/demos/audio-metering/src/main.js
@@ -1,7 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import '@mdi/font/css/materialdesignicons.css';
+import Vuetify from 'vuetify/lib';
 
-createApp(App).mount('#app')
+Vue.use(Vuetify);
+
+const vuetifyOptions = {
+    theme: {
+        themes: {
+            light: {
+                primary: '#E4454A',
+                secondary: '#9E9E9E',
+                accent: '#E3E05B',
+                error: '#961E22',
+                info: '#2DA0E3',
+                success: '#44E36E'
+            }
+        }
+    }
+};
+
+new Vue({
+    vuetify: new Vuetify(vuetifyOptions),
+    el: '#app',
+    render: h => h(App)
+});
 
 import { audioEngine } from './audio/engine.js';
 const songURLs = import.meta.glob('./assets/testSongs/*.mp3');
@@ -17,6 +40,10 @@ async function retrieveTestSongs () {
             break;
         }
     }
+    // const decorrAudio = "https://cdn.glitch.global/9ac995f5-cf01-4f58-8c52-3f6865837ebc/phase-test-decorr.mp3?v=1645544275141"; 
+    // const freesoundBeat = "https://freesound.org/data/previews/234/234809_1648664-lq.mp3";
+    // let song = await fetch(freesoundBeat).then(resp => resp.blob());
+    // songs.push(song);
     return songs;
 }
 
--- examples/demos/audio-metering/vite.config.js
+++ examples/demos/audio-metering/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2';
+import Components from 'unplugin-vue-components/vite'; 
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin()],
+  plugins: [
+    createVuePlugin(),
+    Components({
+      resolvers: [VuetifyResolver()]
+    })
+  ],
   test: {
     environment: 'happy-dom'
   }
