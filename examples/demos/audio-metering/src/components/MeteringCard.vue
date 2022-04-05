994bca28be67c01284df3626eae46ea5d7767f4a
Jorge Marcos <jorge.marcosfer@gmail.com>
2022-04-05 17:17:34 +0200

    extract metering card component

---
+++ examples/demos/audio-metering/src/components/MeteringCard.vue
@@ -0,0 +1,48 @@
+<template>
+	<v-card max-width="500">
+		<v-toolbar elevation="0" dense>
+			<v-btn icon color="primary" text>
+				<v-icon>mdi-tray-arrow-down</v-icon>
+			</v-btn>
+			<v-toolbar-title class="primary--text">{{ track.name }}</v-toolbar-title>
+		</v-toolbar>
+		<v-divider></v-divider>
+		<v-container fluid>
+			<v-row dense>
+				<v-col cols="12">
+					<loudness-card
+						:integrated="track.loudness.integrated"
+						:range="track.loudness.range"
+						:rms="track.loudness.rms"
+						:momentary="track.loudness.momentary"
+						:shortTerm="track.loudness.shortTerm"
+						:trackname="track.name"
+					></loudness-card>
+				</v-col>
+				<v-col cols="12">
+					<phase-card 
+						:leftCh="track.phase.channelData[0]"
+						:rightCh="track.phase.channelData[1]"
+						:correlation="track.phase.correlation"
+					></phase-card>
+				</v-col>
+				<v-col cols="12">
+					<spectral-card :data="track.spectralProfile.integrated" :trackname="track.name"></spectral-card>
+				</v-col>
+			</v-row>
+		</v-container>
+	</v-card>
+</template>
+
+<script>
+import LoudnessCard from './LoudnessCard.vue';
+import PhaseCard from './PhaseCard.vue';
+import SpectralCard from './SpectralCard.vue';
+
+export default {
+	props: {
+		track: Object
+	},
+	components: {LoudnessCard, PhaseCard, SpectralCard}
+}
+</script>
--- examples/demos/audio-metering/src/components/ResultsScreen.vue
+++ examples/demos/audio-metering/src/components/ResultsScreen.vue
@@ -9,53 +9,17 @@
         v-for="track in AnalysisData"
         :key="track.name"
       >
-		<!-- <v-col v-for="track in AnalysisData" :key="track.name"> -->
-				<v-card max-width="500">
-					<v-toolbar elevation="0" dense>
-						<v-btn icon color="primary" text>
-							<v-icon>mdi-tray-arrow-down</v-icon>
-						</v-btn>
-						<v-toolbar-title class="primary--text">{{ track.name }}</v-toolbar-title>
-					</v-toolbar>
-					<v-divider></v-divider>
-					<v-container fluid>
-						<v-row dense>
-							<v-col cols="12">
-								<loudness-card
-									:integrated="track.loudness.integrated"
-									:range="track.loudness.range"
-									:rms="track.loudness.rms"
-									:momentary="track.loudness.momentary"
-									:shortTerm="track.loudness.shortTerm"
-									:trackname="track.name"
-								></loudness-card>
-							</v-col>
-							<v-col cols="12">
-								<phase-card 
-									:leftCh="track.phase.channelData[0]"
-									:rightCh="track.phase.channelData[1]"
-									:correlation="track.phase.correlation"
-								></phase-card>
-							</v-col>
-							<v-col cols="12">
-								<spectral-card :data="track.spectralProfile.integrated" :trackname="track.name"></spectral-card>
-							</v-col>
-						</v-row>
-					</v-container>
-				</v-card>
+				<metering-card :track="track"></metering-card>
 			</v-slide-item>
     </v-slide-group>
   </v-row>
-		<!-- </v-col> -->
 </template>
 
 <script>
-import LoudnessCard from './LoudnessCard.vue';
-import PhaseCard from './PhaseCard.vue';
-import SpectralCard from './SpectralCard.vue';
+import MeteringCard from './MeteringCard.vue';
 
 export default {
-	components: {LoudnessCard, PhaseCard, SpectralCard},
+	components: { MeteringCard },
 	props: {
 		AnalysisData: Array[Object]
 	},
@@ -65,7 +29,14 @@ export default {
 		}
 	},
 	methods: {
-
+		// formatName (name) {
+		// 	const startNumRegexp = /^\d+/;
+		// 	const tempstr = name.replace(reg, '');
+		// 	return tempstr.replace(' ', '-');
+		// },
+		// handlePanelClick (name) {
+			
+		// }
 	}
 }
 </script>
